94a4ece1647338a479426f6a7a47be199176a083
Sean Monaghan <smonagha@conncoll.edu>
2021-02-19 17:48:38 -0500

    set variables and create setTimeOfDay function

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+let currentDayText = document.getElementById('today');
+let DateTime = luxon.DateTime;
+let timeOfDay = document.getElementById('timeOfDay');
+
+
+
+let today = DateTime.local();
+let f = {month: 'long', day: '2-digit'};
+let m = today.get('month');
+let h = today.get('hour');
+let tz = today.zoneName;
+
+currentDayText.textContent = today.toLocaleString();
+
+// Greeting Conditional
+function setTimeOfDay() {
+    if (h > 18) {
+        timeOfDay.textContent = "Evening";
+    } else if (h < 12) {
+        timeOfDay.textContent = "Morning";
+    } else {
+        timeOfDay.textContent = "Day";
+    };
+};
+
+
+setTimeOfDay();
+
