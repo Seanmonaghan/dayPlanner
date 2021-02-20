0f9368872829a5d0da69b7065fcfebd0e36f49df
Sean Monaghan <smonagha@conncoll.edu>
2021-02-19 22:37:43 -0500

    altered hourly divs to forms for easier submitting and fixed CSS to keep everything in line even when small

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,12 @@ let DateTime = luxon.DateTime;
 let timeOfDay = $('#timeOfDay');
 let currentDayText = $("#today");
 let today = DateTime.local();
-let saveButtons = $(".btn");
+let saveButtons = $("button");
 let currentTime = $("#currentTime");
 let nine = $(".09");
 let images = $(".timeImage");
+let liEl = $("li");
+let divEl = $("div");
 
 
 
@@ -18,15 +20,23 @@ let m = today.get('month');
 let h = today.get('hour');
 let currentTimeText = today.toLocaleString(f);
 
-saveButtons.click(function(){
+saveButtons.click(function(event){
+    event.preventDefault;
     $('.09').append($('<li>', {
          text: $('#09input').val()
     }));
 });
 
+$("body").on('click', 'li', function() {
+    $(this).css("text-decoration", "line-through");
+});
 
 
 
+divEl.on('click', function(e) {
+    e.stopPropagation;
+    console.log($(this));
+});
 
 
 // Greeting Conditional
@@ -35,12 +45,15 @@ function setTimeOfDay() {
     currentTime.text(currentTimeText);
     if (h > 18) {
         timeOfDay.text("Evening");
+        $("body").css("background-color", "grey");
         images.prepend($('<img>',{id:'moon',src:'http://clipart-library.com/images_k/moon-clipart-transparent/moon-clipart-transparent-10.png'}));
     } else if (h < 12) {
         timeOfDay.text("Morning");
+        $("body").css("background-color", "lightblue");
         images.prepend($('<img>',{id:'risingSun',src:'https://www.pikpng.com/pngl/m/2-24499_28-collection-of-sunrise-clipart-transparent-clip-art.png'}));
     } else {
         timeOfDay.text("Day");
+        $("body").css("background-color", "orange");
         images.prepend($('<img>',{id:'sun',src:'http://clipart-library.com/image_gallery/614668.gif'}));
     }
 };
