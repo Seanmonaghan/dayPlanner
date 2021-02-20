f756aa6c3f759cd82b65b61362662095d3281242
Sean Monaghan <smonagha@conncoll.edu>
2021-02-19 21:11:44 -0500

    added clip art for times of day, and set up basic input structure to add bullet points per day

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,53 @@
-let currentDayText = document.getElementById('today');
+// Luxon Variable for convenience
 let DateTime = luxon.DateTime;
-let timeOfDay = document.getElementById('timeOfDay');
 
-
-
+// HTML Elements
+let timeOfDay = $('#timeOfDay');
+let currentDayText = $("#today");
 let today = DateTime.local();
-let f = {month: 'long', day: '2-digit'};
+let saveButtons = $(".btn");
+let currentTime = $("#currentTime");
+let nine = $(".09");
+let images = $(".timeImage");
+
+
+
+// Time Variables
+let f = {hour: 'numeric', minute: '2-digit'};
 let m = today.get('month');
 let h = today.get('hour');
-let tz = today.zoneName;
+let currentTimeText = today.toLocaleString(f);
 
-currentDayText.textContent = today.toLocaleString();
+saveButtons.click(function(){
+    $('.09').append($('<li>', {
+         text: $('#09input').val()
+    }));
+});
+
+
+
+
+
 
 // Greeting Conditional
 function setTimeOfDay() {
+    currentDayText.text(today.toLocaleString());
+    currentTime.text(currentTimeText);
     if (h > 18) {
-        timeOfDay.textContent = "Evening";
+        timeOfDay.text("Evening");
+        images.prepend($('<img>',{id:'moon',src:'http://clipart-library.com/images_k/moon-clipart-transparent/moon-clipart-transparent-10.png'}));
     } else if (h < 12) {
-        timeOfDay.textContent = "Morning";
+        timeOfDay.text("Morning");
+        images.prepend($('<img>',{id:'risingSun',src:'https://www.pikpng.com/pngl/m/2-24499_28-collection-of-sunrise-clipart-transparent-clip-art.png'}));
     } else {
-        timeOfDay.textContent = "Day";
-    };
+        timeOfDay.text("Day");
+        images.prepend($('<img>',{id:'sun',src:'http://clipart-library.com/image_gallery/614668.gif'}));
+    }
 };
 
 
+function init() {
+    setTimeOfDay();
+}
 
-setTimeOfDay();
-
+init();
